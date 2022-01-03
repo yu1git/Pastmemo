35d3b739975985d8d5175d860883f4c9f1d54b36
yu1git <mitateyuu@gmail.com>
2022-01-03 18:42:36 +0900

    crud

--- resources/js/pages/MemoForm.vue
+++ resources/js/pages/MemoForm.vue
@@ -1,59 +1,52 @@
 <template>
-    <div class="memo-form">
-        <form 
-            class=""
-            @submit.prevent="addNewMemo"
+  <div class="memo-form">
+    <form class="" @submit.prevent="addNewMemo">
+      <nav class="d-flex bd-highlight mb-3">
+        <h2 class="title p-2 flex-grow-1 bd-highlight">New Memo</h2>
+        <button
+          @click="ret"
+          type="reset"
+          class="btn btn-outline-info p-2 m-2 bd-highlight"
         >
-        <nav class="d-flex bd-highlight mb-3">
-            <h2 class="title p-2 flex-grow-1 bd-highlight">New Memo</h2>
-            <button
-                @click="ret"
-                type="reset"
-                class="btn btn-outline-info p-2 m-2 bd-highlight"
-            >
-                キャンセル
-            </button>
-            <button 
-                type="submit"
-                class="btn btn-outline-info p-2 m-2 bd-highlight"
-            >
-                新規メモ作成
-            </button>
-            
-        </nav>
-        <br>
-        
-        <div class="p-3 d-flex flex-column memo-box">
-            <input
-                class="memo-title h5"
-                style="border:none;"
-                type="text"
-                v-model="newMemo.title" 
-                placeholder="タイトル"
-                @keyup.enter="$event.target.nextElementSibling.focus()"
-            />
-            <textarea
-                id="2"
-                class="memo-content"
-                style="border:none; resize: none;"
-                rows="10"
-                v-model="newMemo.content" 
-                placeholder="ここからメモを書きましょう。"
-            ></textarea> 
-            <div v-show="errorMessage">
-                <span class="text-xs text-red-500">
-                {{ errorMessage }}
-                </span>
-            </div>
+          戻る
+        </button>
+        <button type="submit" class="btn btn-outline-info p-2 m-2 bd-highlight">
+          新規メモ作成
+        </button>
+      </nav>
+      <br />
+
+      <div class="p-3 d-flex flex-column memo-box">
+        <input
+          class="memo-title h5"
+          style="border: none"
+          type="text"
+          v-model="newMemo.title"
+          placeholder="タイトル"
+          @input="flagChange"
+          @keyup.enter="$event.target.nextElementSibling.focus()"
+        />
+        <textarea
+          class="memo-content"
+          style="border: none; resize: none"
+          rows="10"
+          v-model="newMemo.content"
+          placeholder="ここからメモを書きましょう。"
+          @input="flagChange"
+        ></textarea>
+        <div v-show="errorMessage">
+          <span class="text-xs text-red-500">
+            {{ errorMessage }}
+          </span>
         </div>
-        </form>
-        
-    </div>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
-import axios from 'axios'
-import {baseurl} from '../const'
+import axios from "axios";
+import { baseurl } from "../const";
 
 export default {
   // props: {
@@ -65,53 +58,76 @@ export default {
         title: "",
         content: "",
       },
-      errorMessage: ""
+      errorMessage: "",
+      flag: false,
     };
   },
-  //mounted() {
-    // const url = baseurl + 'api/memos'
-    // axios.post(url,{
-    //   title:'テスト',
-    //   content:'内容',
-    // }).then((response) => {
-    //   // axiosが成功したときのHTTPレスポンスを表示
-    //   console.log(response)
-    // }).catch((error) => {
-    //   // axiosが失敗したときのエラーを表示
-    //   console.log(error)
-    // })
-    // this.newMemo.id = this.id;
+  // created() {
+  //   window.addEventListener('popstate', this.backAlert);
+  // },
+  created() {
+    window.addEventListener("beforeunload", this.confirmSave);
+  },
+  destroyed() {
+    window.removeEventListener("beforeunload", this.confirmSave);
+  },
+  beforeRouteLeave(to, from, next) {
+    if (this.flag) {
+      const answer = window.confirm(
+        "編集中のものは保存されませんが、よろしいですか？"
+      );
+      if (answer) {
+        next();
+      } else {
+        next(false);
+      }
+    } else {
+      next();
+    }
+  },
+  
+  // mounted() {
+  // const url = baseurl + 'api/memos'
+  // axios.post(url,{
+  //   title:'テスト',
+  //   content:'内容',
+  // }).then((response) => {
+  //   // axiosが成功したときのHTTPレスポンスを表示
+  //   console.log(response)
+  // }).catch((error) => {
+  //   // axiosが失敗したときのエラーを表示
+  //   console.log(error)
+  // })
+  // this.newMemo.id = this.id;
   // },
   methods: {
     addNewMemo() {
-      if (!this.newMemo.title) {
-        this.errorMessage = "タイトルを記入してください";
+      this.flag = false;
+      if (!this.newMemo.content) {
+        this.errorMessage = "本文を記入してください";
         return;
       }
-      const url = baseurl + 'api/memos';
+      const url = baseurl + "api/memos";
       axios
         .post(url, this.newMemo)
-        .then(res => {
+        .then((res) => {
           // this.newMemo.title = "";
           // this.newMemo.content = "";
           //this.$emit("Memo-added", res.data);
-          console.log(res)
+          //console.log(res);
+          this.$router.push({ name: "MemoList" });
         })
-        .catch(err => {
+        .catch((err) => {
           this.handleErrors(err);
         });
-      //新規追加したら、MemoListに戻る
-      this.$router.push({
-        name: 'MemoList'
-      })
     },
     handleErrors(err) {
       if (err.response && err.response.status === 422) {
         const errorBag = err.response.data.errors;
         if (errorBag.title) {
           this.errorMessage = errorBag.title[0];
-        } else if (errorBag.description) {
-          this.errorMessage = errorBag.description[0];
+        } else if (errorBag.content) {
+          this.errorMessage = errorBag.content[0];
         } else {
           this.errorMessage = err.response.message;
         }
@@ -122,24 +138,66 @@ export default {
     //キャンセルの時は文章リセットして戻る
     ret() {
       this.$router.push({
-        name: 'MemoList'
-      })
-    }
+        name: "MemoList",
+      });
+    },
+    flagChange() {
+      this.flag = true;
+    },
+    confirmSave(event) {
+      if (this.flag) {
+        event.returnValue = "編集中のものは保存されませんが、よろしいですか？";
+      }
+    },
+    // backAlert() {
+    //   // if (!this.newMemo.content) {
+    //   //       this.errorMessage = "本文を記入してください";
+
+    //   //     }
+    //   if(this.flag){
+    //     //this.errorMessage = "変更された内容があります。保存しますか？"
+    //     if(window.confirm('変更された内容があります。保存しますか？')){
+    //     //if(){
+    //       this.flag = false;
+    //       if (!this.newMemo.content) {
+    //         const ans = window.confirm('本文を記入してください');
+    //         if(ans){
+    //           this.$router.push({ name: "MemoForm" });
+    //         }
+    //       }
+    //       this.addNewMemo();
+    //     }else{
+    //       this.flag = false;
+    //       this.ret();
+    //     };
+    //   }
+    // }
   },
 };
 </script>
 <style scoped>
-.memo-box{
+.memo-box {
   color: #696969;
   font-weight: bold;
   border: solid 1px #e6e6e6;
-  box-shadow:2px 2px 0 rgba(0,0,0,.1);
+  box-shadow: 2px 2px 0 rgba(0, 0, 0, 0.1);
 }
-.memo-content{
+.memo-content {
   background-color: #fff;
-  background-image:
-  linear-gradient(90deg, rgba(237, 119, 128, 0) 0%, rgba(237, 119, 128, 0) 50%, #fff 0%, #fff 100%), linear-gradient(180deg, rgba(100, 100, 100, 0) 0%, rgba(100, 100, 100, 0) 97.5%, #646464 100%);
-  background-size: 8px 100%,100% 2rem;
+  background-image: linear-gradient(
+      90deg,
+      rgba(237, 119, 128, 0) 0%,
+      rgba(237, 119, 128, 0) 50%,
+      #fff 0%,
+      #fff 100%
+    ),
+    linear-gradient(
+      180deg,
+      rgba(100, 100, 100, 0) 0%,
+      rgba(100, 100, 100, 0) 97.5%,
+      #646464 100%
+    );
+  background-size: 8px 100%, 100% 2rem;
   line-height: 2rem;
   padding: 2rem 1rem 0.2rem 1rem;
 }
--- resources/js/pages/MemoList.vue
+++ resources/js/pages/MemoList.vue
@@ -4,15 +4,28 @@
       <div class="col-md-9">
         <h1 class="text-center m-3">Memo List</h1>
         <div class="row">
-          <div class="col-sm-2 col-xs-6 mb-3" style="width: 12rem;" v-for="memo in $store.state.memos" v-bind:key="memo.id">
-            <div class="card" style="height:12rem;">
-              <div class="card-body">
-                <h6 class="card-title">{{ memo.title }}</h6>
-                <p class="card-text">{{ memo.content }}</p>
-                <div class="text-end">
-                  <small class="card-text">{{ makeDate(memo.updated_at) }}</small>
+          <div
+            class="col-sm-2 col-xs-6 mb-3"
+            style="width: 12rem"
+            v-for="memo in $store.state.memos"
+            v-bind:key="memo.id"
+          >
+            <div class="card" style="height: 12rem">
+              <router-link
+                :to="{ name: 'MemoEdit', params: { id: memo.id } }"
+                style="text-decoration: none"
+                class="text-body"
+              >
+                <div class="card-body">
+                  <h6 class="card-title">{{ memo.title }}</h6>
+                  <p class="card-text">{{ memo.content }}</p>
+                  <div class="text-end">
+                    <small class="card-text">{{
+                      makeDate(memo.updated_at)
+                    }}</small>
+                  </div>
                 </div>
-              </div>
+              </router-link>
             </div>
           </div>
         </div>
@@ -25,27 +38,26 @@
 </template>
 
 <script>
-import PastMemo from '../components/PastMemo.vue'
-import dayjs from 'dayjs'
+import PastMemo from "../components/PastMemo.vue";
+import dayjs from "dayjs";
 
 export default {
-    name: 'memoList',
-    components:{
-        PastMemo,
+  name: "MemoList",
+  components: {
+    PastMemo,
+  },
+  methods: {
+    makeDate(date) {
+      return dayjs(date).format("YYYY/MM/DD HH:mm");
     },
-    methods:{
-      makeDate(date){
-        return dayjs(date).format('YYYY/MM/DD HH:mm')
-      }
-    },
-    // computed: {
-    //     memos: ()=>{
-    //     this.$store.getters.memos
-    //     }
-    // },
-    mounted(){
-      this.$store.dispatch('getMemos');
-    }
-    
-}
+  },
+  // computed: {
+  //     memos: ()=>{
+  //     this.$store.getters.memos
+  //     }
+  // },
+  mounted() {
+    this.$store.dispatch("getMemos");
+  },
+};
 </script>
