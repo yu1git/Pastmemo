150314de50ec49990b4d80f8b8991d8d8692f1d4
yu1git <mitateyuu@gmail.com>
2022-01-16 11:54:24 +0900

    memo

--- config/cors.php
+++ config/cors.php
@@ -15,20 +15,28 @@ return [
     |
     */
 
+    //許可するCORSヘッダーを出力するパスの設定
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
+    //許可するHTTPメソッドの設定
     'allowed_methods' => ['*'],
 
+    //許可するドメインの設定
     'allowed_origins' => ['*'],
 
+    //許可するドメインのパターン
     'allowed_origins_patterns' => [],
 
+    //許可するヘッダーの設定
     'allowed_headers' => ['*'],
 
+    //レスポンスヘッダーの公開指定
     'exposed_headers' => [],
 
+    //ブラウザのキャッシュの保管期間
     'max_age' => 0,
 
+    //クッキーなどの認証の許可
     'supports_credentials' => false,
 
 ];
