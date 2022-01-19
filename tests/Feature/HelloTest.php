c98a318f834ed8a37a2ed071bc32d931a862401e
yu1git <mitateyuu@gmail.com>
2022-01-19 14:07:40 +0900

    test

--- tests/Feature/HelloTest.php
+++ tests/Feature/HelloTest.php
@@ -5,49 +5,70 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
 use App\Models\User;
 
 class HelloTest extends TestCase
 {
-    use RefreshDatabase;
+    // use RefreshDatabase;
 
-    private $accessToken = null;
+    // private $accessToken = null;
 
-    public function setUp(): void
-    {
-        parent::setUp();
+    // public function setUp(): void
+    // {
+    //     parent::setUp();
 
-        // テストユーザ作成
-        User::create([
-            'name' => 'sample user',
-            'email' => 'sample@sankosc.co.jp',
-            'password' => Hash::make('sample123'),
-        ]);
+    //     // テストユーザ作成
+    //     User::create([
+    //         'name' => 'sample user',
+    //         'email' => 'sample@sankosc.co.jp',
+    //         'password' => Hash::make('sample123'),
+    //     ]);
 
-        // 2.ログインAPIでアクセストークン取得
-        $response = $this->post('/api/login', [
-            'email' => 'sample@sankosc.co.jp',
-            'password' => 'sample123'
-        ]);
-        $response->assertOk();
-        // 3.アクセストークンを変数に保存しておく
-        $this->accessToken = $response->decodeResponseJson('access_token');
-    }
+    //     // 2.ログインAPIでアクセストークン取得
+    //     $response = $this->post('/api/login', [
+    //         'email' => 'sample@sankosc.co.jp',
+    //         'password' => 'sample123'
+    //     ]);
+    //     $response->assertOk();
+    //     // 3.アクセストークンを変数に保存しておく
+    //     $this->accessToken = $response->decodeResponseJson('access_token');
+    // }
 
+    /**
+    * A basic feature test example.
+    *
+    * @return void
+    */
+//    public function test_正しいパスワードの場合()
+//    {
+//        $response = $this->get('api/login');          
+//        $response->assertStatus(200);
+
+//        // ログインする
+//       $response = $this->post('api/login', ['email' => $this->user->email, 'password' => 'Test1234']);
+//        // リダイレクトでページ遷移してくるのでstatusは302
+//       $response->assertStatus(302);
+//        // リダイレクトで帰ってきた時のパス
+//       $response->assertRedirect('/');
+//        // このユーザーがログイン認証されているか
+//       $this->assertAuthenticatedAs($this->user);
+
+//    }
      /**
      * @test
      * @group testing
      */
-    public function getWithAuth()
-    {
-        $response = $this->get('/api/user', [
-            'Authorization' => 'Bearer '.$this->accessToken
-        ]);
-        $response->assertOk()->assertJsonFragment([
-            'name' => 'sample user'
-        ]);
-    }
+    // public function getWithAuth()
+    // {
+    //     $response = $this->get('/api/user', [
+    //         'Authorization' => 'Bearer '.$this->accessToken
+    //     ]);
+    //     $response->assertOk()->assertJsonFragment([
+    //         'name' => 'sample user'
+    //     ]);
+    // }
     /**
      * ログイン認証テスト
      */
@@ -84,5 +105,21 @@ class HelloTest extends TestCase
 
     //     // ユーザーが認証されていないことを確認
     //     $this->assertGuest();
+    // }
+
+    
+    /** Authファサードをモックする方法@test */
+    // public function ログインしていない場合のテスト()
+    // {
+    //     Auth::shouldReceive('check')->andReturn(false);
+    //     $response = $this->get('/');
+    //     $response->assertViewHas('data', 'ログインしていません');
+    // }
+    // /** @test */
+    // public function ログインしている場合のテスト()
+    // {
+    //     Auth::shouldReceive('check')->andReturn(true);
+    //     $response = $this->get('/');
+    //     $response->assertViewHas('data', 'ログインしています');
     // }
 }
