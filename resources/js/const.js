03c75d664cc8daeaad4e1021b6a7c2e3a6ed56a3
yu1git <mitateyuu@gmail.com>
2022-01-20 18:16:18 +0900

    baseurl

--- resources/js/const.js
+++ resources/js/const.js
@@ -1,3 +1,7 @@
-export const baseurl = 'http://127.0.0.1:8000/'
+//ローカル
+//export const baseurl = 'http://127.0.0.1:8000/'
+
+//本番　Heroku
+export const baseurl = 'https://pastmemo.herokuapp.com/'
 
 export default baseurl
