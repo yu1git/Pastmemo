1ed23344acabd490e0d3e2bef416a27749b344b2
yu1git <mitateyuu@gmail.com>
2022-01-20 15:14:34 +0900

    Herokuデプロイ

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        # 本番環境でHTTPS化
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
