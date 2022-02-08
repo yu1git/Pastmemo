65fab75c3311b51dfd33f1d90e02b7b731d7867e
yu1git <mitateyuu@gmail.com>
2022-02-08 14:30:58 +0900

    ext-intl

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -47,8 +47,8 @@ class RegisterController extends Controller
 
     // 会員登録
     public function register(RegisterRequest $request) {
-        $message = "3RegisterControllerまできた";
-        dd($message,$request);
+        //$message = "3RegisterControllerまできた";
+        //dd($message,$request);
         User::create([
             'name' =>  $request->name,
             'email' => $request->email,
